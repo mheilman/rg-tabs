fb92337078a59f79f340bc263632d6da2798d50e
Greg <greg@flashtalking.com>
2015-05-21 14:29:03 +0100

    Re-worked headings decoupled from tags

---
+++ dist/rg-tabs.js
@@ -0,0 +1,44 @@
+riot.tag('rg-tab', '<div show="{ active }" class="tab"> <yield></yield> </div>', '.tab { padding: 20px; }', function (opts) {
+	var _this = this;
+	_this.active = opts.active;
+	_this.disabled = opts.disabled;
+
+});
+
+riot.tag('rg-tabs', '<div class="tabs"> <div class="headers"> <div each="{ tabs }" class="header { active: active, disabled: disabled }" onclick="{ parent.activate }"> <h4>{ opts.heading }</h4> </div> </div> <yield></yield> </div>', 'rg-tabs .tabs { background-color: white; } rg-tabs .headers { display: flex; } rg-tabs .header { flex: 1; box-sizing: border-box; text-align: center; cursor: pointer; box-shadow: 0 -1px 0 0 #6495ED inset; } rg-tabs .header.active { background-color: #6495ED; color: white; } rg-tabs .header.disabled { color: grey; }', function (opts) {
+	var _this = this;
+	_this.tabs = _this.tags['rg-tab'];
+
+	_this.tabs.forEach(function (tab, i) {
+		tab.index = i;
+	});
+
+	_this.on('mount', function () {
+		var activeTab = false;
+		_this.tabs.forEach(function (tab) {
+			if (activeTab) {
+				tab.active = false;
+			}
+			if (tab.active) {
+				activeTab = true;
+			}
+		});
+	});
+
+	_this.activate = function (tab) {
+		tab = tab.item;
+		if (!tab.disabled) {
+			deselectTabs();
+			opts.onopen(tab);
+			tab.active = true;
+			_this.update();
+		}
+	};
+
+	function deselectTabs() {
+		_this.tabs.forEach(function (tab) {
+			tab.active = false;
+		});
+	}
+
+});
